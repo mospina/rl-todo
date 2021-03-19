6e3221721ea689adf7f44e34c3f3530e00a2d78a
Manuel <manuel@nativshark.com>
2021-03-19 13:47:45 +0900

    Add styled components to form

--- src/components/Tasks/Form/index.tsx
+++ src/components/Tasks/Form/index.tsx
@@ -1,4 +1,13 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const priorityOptions = [
+  { label: "highest", value: 1 },
+  { label: "high", value: 2 },
+  { label: "medium", value: 3 },
+  { label: "low", value: 4 },
+  { label: "lowest", value: 5 },
+];
 
 const Form = ({ onSubmit }: Tasks.FormProps) => {
   const [content, setContent] = useState<string>("");
@@ -21,39 +30,86 @@ const Form = ({ onSubmit }: Tasks.FormProps) => {
 
   const handlePriorityChange = ({
     target,
-  }: React.ChangeEvent<HTMLInputElement>) => {
+  }: React.ChangeEvent<HTMLSelectElement>) => {
     const newValue = Number(target.value);
-    if (newValue <= 0) {
-      setError("Priority must be higher than 0");
-      return;
-    }
 
     setPriority(newValue);
     setError(null);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <FormContainer onSubmit={handleSubmit}>
       {error && <div className="error">{error}</div>}
-      <label htmlFor="Content">Task: </label>
-      <input
-        id="content"
-        type="text"
-        value={content}
-        name="Content"
-        onChange={({ target }) => setContent(target.value)}
-      />
-      <label htmlFor="Priority">Priority: </label>
-      <input
-        id="priority"
-        type="number"
-        value={priority}
-        name="Priority"
-        onChange={handlePriorityChange}
-      />
+      <FormField>
+        <label htmlFor="Content">Task: </label>
+        <input
+          id="content"
+          type="text"
+          value={content}
+          name="Content"
+          onChange={({ target }) => setContent(target.value)}
+        />
+      </FormField>
+      <FormField>
+        <label htmlFor="Priority">Priority: </label>
+        <Select value={priority} onChange={handlePriorityChange}>
+          {priorityOptions.map((p) => (
+            <option value={p.value}>{p.label}</option>
+          ))}
+        </Select>
+      </FormField>
       <button type="submit">Submit</button>
-    </form>
+    </FormContainer>
   );
 };
+
+const FormContainer = styled.form`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+`;
+
+const FormField = styled.div`
+  padding: 0 1rem 0 1rem;
+`;
+
+const Select = styled.select`
+  display: inline-block;
+  width: auto;
+  padding: 0.6em 1.4em 0.5em 0.8em;
+  width: auto;
+  box-sizing: border-box;
+  margin: 0;
+  border: 1px solid #aaa;
+  box-shadow: 0 1px 0 1px rgba(0, 0, 0, 0.04);
+  border-radius: 0.5em;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  appearance: none;
+  background-color: #fff;
+  background-image: url("data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E"),
+    linear-gradient(to bottom, #ffffff 0%, #e5e5e5 100%);
+  background-repeat: no-repeat, repeat;
+  background-position: right 0.7em top 50%, 0 0;
+  background-size: 0.65em auto, 100%;
+
+  &:hover {
+    border-color: #888;
+  }
+
+  &:focus {
+    border-color: #aaa;
+    box-shadow: 0 0 1px 3px #e8d5b5;
+    box-shadow: 0 0 0 3px -moz-mac-focusring;
+    color: #222;
+    outline: none;
+  }
+
+  & option {
+    font-weight: normal;
+  }
+`;
 
 export default Form;
